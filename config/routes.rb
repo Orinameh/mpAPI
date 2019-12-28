1ede80cf21d3dc9356a02fa36d7372a1424051a9
David Evhade <david@kobo360.com>
2019-12-28 15:23:40 +0100

    Create user model and run first test

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # API definition
+  namespace :api, defaults: { format: :json}, constraints: { subdomain: 'api'}, path: '/' do
+    scope module: :v1 do
+    end
+  end
+end
