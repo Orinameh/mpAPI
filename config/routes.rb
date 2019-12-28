0ac1296cafac74fb837e4a6995a702f309f02d4c
David Evhade <david@kobo360.com>
2019-12-28 17:40:00 +0100

    adds show action to users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   # API definition
-  namespace :api, defaults: { format: :json}, constraints: { subdomain: 'api'}, path: '/' do
-    scope module: :v1 do
-    end
+  # namespace :api, defaults: { format: :json}, constraints: { subdomain: 'api'}, path: '/' do
+  #   scope module: :v1 do
+  #   end
+  # end
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :users, only: [:show]
+    end 
   end
 end
