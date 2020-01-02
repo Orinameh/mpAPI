82889a5b3c9916b2ab66d339c4dae8331562e301
David Evhade <david@kobo360.com>
2020-01-02 19:04:34 +0100

    Adds authenticable module for managing authentication methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+
+    # From the concerns folder
+    include Authenticable
 end
--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,7 @@ module MarketPlaceApi
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
+  
     config.eager_load_paths << Rails.root.join('lib/tasks')
   end
 end
