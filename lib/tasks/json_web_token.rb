3a7ee9f057759df365d4638e78a0f5fb1e261bbb
David Evhade <david@kobo360.com>
2020-01-02 13:47:42 +0100

    set up jwt gem

---
+++ lib/tasks/json_web_token.rb
@@ -0,0 +1,16 @@
+class JsonWebToken
+    SECRET_KEY = Rails.application.secrets.secret_key_base.to_s
+    
+    # class methods start with self while instance methods just have their names without self
+    def self.encode(payload, exp = 24.hours.from_now)
+        payload[:exp] = exp.to_i
+        JWT.encode(payload, SECRET_KEY)
+    end
+
+    def self.decode(token)
+        decoded = JWT.decode(token, SECRET_KEY).first
+        HashWithIndifferentAccess.new decoded
+    end
+end
+
+# autoload this to the application.rb file
