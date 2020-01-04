f3a58d59a79391a219f4facebbb68b14401ea3d7
David Evhade <david@kobo360.com>
2020-01-05 00:04:18 +0100

    adds product validations

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
       # resources :users, only: [:show, :create]
       resources :users, only: %i[show create update destroy]
       resources :tokens, only: [:create]
+      resources :products, only: [:show]
     end 
   end
 end
