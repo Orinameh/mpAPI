555daccb859ef989043eeab6b86ef9fcda4cf220
David Evhade <david@kobo360.com>
2020-01-05 00:09:05 +0100

    finish modelling product and user associations

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
       # resources :users, only: [:show, :create]
       resources :users, only: %i[show create update destroy]
       resources :tokens, only: [:create]
-      resources :products, only: [:show]
+      resources :products, only: %i[show index]
     end 
   end
 end
