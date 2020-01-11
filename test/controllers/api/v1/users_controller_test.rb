e25c5ba5cd210d93104d47be7fc640a7740c7dcb
David Evhade <david@kobo360.com>
2020-01-11 10:00:34 +0100

    Add products relationship to user#show

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -16,6 +16,7 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     json_response = JSON.parse(self.response.body, symbolize_names: true)
     assert_equal @user.email, json_response.dig(:data, :attributes, :email)
     assert_equal @user.products.first.id.to_s, json_response.dig(:data, :relationships, :products, :data, 0, :id)
+    assert_equal @user.products.first.title, json_response.dig(:included, 0, :attributes, :title)
   end
 
 
