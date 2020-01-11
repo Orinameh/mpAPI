91e20814bcc24e6069c63aaeb27dd49ee9676a4d
David Evhade <david@kobo360.com>
2020-01-11 11:04:36 +0100

    added query to filter products  by title

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,8 @@ class Product < ApplicationRecord
   validates :title, :user_id, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0}, presence: true
   belongs_to :user
+
+  # Adds a class method for retrieving and querying objects.
+  # scoping allows you to specify commonly used queries that can be referenced as method calls on models.
+  scope :filter_by_title, lambda {|keyword| where('lower(title) LIKE ?', "%#{keyword.downcase}%") }
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -6,4 +6,14 @@ class ProductTest < ActiveSupport::TestCase
     product.price = -1
     assert_not product.valid?
   end
+
+  test 'should filter products by name' do
+    assert_equal 2, Product.filter_by_title('TV').count
+  end
+
+  test 'should filter products by name and sort them' do
+    assert_equal [products(:another_tv), products(:one)],
+    Product.filter_by_title('TV').sort
+  end
+
 end
