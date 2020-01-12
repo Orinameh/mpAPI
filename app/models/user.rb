c93d6e3669ead9ea0cc18a87e8c34daaf8762f88
David Evhade <david@kobo360.com>
2020-01-12 18:55:14 +0100

    Associates products and orders with a placements model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
-    validates :email, uniqueness: true
-    validates_format_of :email, with: /@/
-    validates :password_digest, presence: true
     has_many :products, dependent: :destroy
     has_many :orders, dependent: :destroy
 
+    validates :email, uniqueness: true
+    validates_format_of :email, with: /@/
+    validates :password_digest, presence: true
+    
     has_secure_password
 end
