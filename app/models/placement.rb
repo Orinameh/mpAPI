3fa4fca6274949fcb23d8147d5b41e83f7147423
David Evhade <david@kobo360.com>
2020-01-16 15:49:17 +0100

    Decreases the product quantity by the placement quantity

--- app/models/placement.rb
+++ app/models/placement.rb
@@ -1,4 +1,11 @@
 class Placement < ApplicationRecord
   belongs_to :order
   belongs_to :product, inverse_of: :placements
+
+  after_create :decrement_product_quantity!
+
+  def decrement_product_quantity!
+    # decrement! helps to skip callbacks
+    product.decrement!(:quantity, quantity)
+  end
 end
