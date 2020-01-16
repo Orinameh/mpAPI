e75853b631c0d7cb969af2a33ead4af2e6608ba1
David Evhade <david@kobo360.com>
2020-01-16 16:54:40 +0100

    Updates the total calculation for order

--- app/models/order.rb
+++ app/models/order.rb
@@ -13,8 +13,11 @@ class Order < ApplicationRecord
   has_many :placements, dependent: :destroy
   has_many :products, through: :placements
 
+  # def set_total!
+  #   self.total = products.map(&:price).sum
+  # end
   def set_total!
-    self.total = products.map(&:price).sum
+    self.total = self.placements.map{ |placement| placement.product.price * placement.quantity }.sum
   end
 
   # @param product_ids_and_quantities [Array<Hash>] something like this `[{product_id: 1, quantity: 2}]`
